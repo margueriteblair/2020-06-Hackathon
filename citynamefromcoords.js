9bb0191b55a1943f8d88bc272f8cf5cf53541093
Marguerite Blair <marguerite.blair@gmail.com>
2020-06-17 20:03:10 -0400

    commented instructions for city name from city coords JS file

---
+++ citynamefromcoords.js
@@ -0,0 +1,4 @@
+//this JS file will be used to program the portion of the API that will be used to get the name of the city from the geolocation API
+//the flow should be:
+//1. end user gets allows program to get their geolocation and geographic coordinates
+//2. we then convert the coordinates to the name of the city.
